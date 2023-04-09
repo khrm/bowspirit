7da28ceef3a5d4da90fb05a318f0df7c5c3a9bf6
Khurram Baig <kbaig@redhat.com>
2023-04-09 17:13:11 +0530

    Reorganize the code

---
+++ lib/home.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+
+class RestDataTable extends StatefulWidget {
+  final Uri apiUrl;
+
+  const RestDataTable({required this.apiUrl});
+
+  @override
+  _RestDataTableState createState() => _RestDataTableState();
+}
+
+class _RestDataTableState extends State<RestDataTable> {
+  late List<Map<String, dynamic>> _data;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  Future<void> _getData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final response = await http.get(widget.apiUrl);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = jsonDecode(response.body);
+      final List<dynamic> result = jsonData['results'];
+      setState(() {
+        _data = result.map((e) => e as Map<String, dynamic>).toList();
+        _isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  List<DataRow> buildRow(List<Map<String, dynamic>> data) {
+      final rows = <DataRow>[];
+
+  // Add rows dynamically
+  for (var row in data) {
+    rows.add(
+      DataRow(cells: [
+        DataCell(Text(row["summary"]["status"].toString())),
+        DataCell(Text("")),
+        DataCell(Text(row["summary"].toString())),
+        DataCell(Text("")),
+        DataCell(Text("")),
+        DataCell(Text(row["uid"].toString())),
+        DataCell(Text(row["createTime"].toString())),
+        DataCell(Text("")),
+      ]),
+    );
+  }
+  return rows;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'State',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Log',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Summary',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Repo',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Revision',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Pipeline',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Started',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Duration',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+      ],
+      rows: buildRow(_data)
+    );
+  }
+}
+
+
