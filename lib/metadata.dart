31a13f4090620dca792e667ad5594ece5f8d2b19
Khurram Baig <kbaig@redhat.com>
2023-04-09 23:13:11 +0530

    Download Button for Pipelinerun YAML

--- lib/metadata.dart
+++ lib/metadata.dart
@@ -1,23 +1,22 @@
+import 'dart:convert';
+import 'dart:html' as html;
+
+import 'package:any_syntax_highlighter/any_syntax_highlighter.dart';
 import 'package:any_syntax_highlighter/themes/any_syntax_highlighter_theme_collection.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-
-import 'package:json2yaml/json2yaml.dart';
-
 import 'package:http/http.dart' as http;
-import 'package:any_syntax_highlighter/any_syntax_highlighter.dart';
+import 'package:json2yaml/json2yaml.dart';
 
 class Metadata extends StatefulWidget {
   final Uri url;
   const Metadata({super.key, required this.url});
-  
+
   @override
   _MetadataState createState() => _MetadataState();
 }
 
 class _MetadataState extends State<Metadata> {
-
-  late Map<String, dynamic>_data;
+  late Map<String, dynamic> _data;
   late String _file;
   bool _isLoading = true;
 
@@ -26,6 +25,7 @@ class _MetadataState extends State<Metadata> {
     super.initState();
     _getData();
   }
+
   Future<void> _getData() async {
     setState(() {
       _isLoading = true;
@@ -35,8 +35,9 @@ class _MetadataState extends State<Metadata> {
       final Map<String, dynamic> jsonData = jsonDecode(response.body);
       setState(() {
         _data = jsonData;
-        var jsonFile = json.decode(utf8.decode(base64.decode( _data["data"]["value"].toString())));
-        _file =  json2yaml(jsonFile);
+        var jsonFile = json.decode(
+            utf8.decode(base64.decode(_data["data"]["value"].toString())));
+        _file = json2yaml(jsonFile);
         _isLoading = false;
       });
     } else {
@@ -46,25 +47,51 @@ class _MetadataState extends State<Metadata> {
 
   @override
   Widget build(BuildContext context) {
-        return _isLoading
+    return _isLoading
         ? const Center(
             child: CircularProgressIndicator(),
           )
         : Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child:SingleChildScrollView(
+            appBar: AppBar(),
+            floatingActionButton: FloatingActionButton.extended(
+                label: const Text('Download'),
+                icon: const Icon(Icons.download_rounded),
+                onPressed: () {
+                  final text = "${_file}";
 
-child: AnySyntaxHighlighter(
-          "${_file}",
-          fontSize: 16,
-          lineNumbers: false, // by default false
-          theme: AnySyntaxHighlighterThemeCollection.defaultLightTheme(), // you can create and pass custom theme using AnySyntaxHighlighterTheme class
-          isSelectableText: true, // this creates a SelectableText.rich() widget, makes text selectable (by default false)
-          useGoogleFont: 'Source Code Pro',
-          copyIcon: const Icon(Icons.copy_rounded,color:Colors.black),// default is white colored icon
-          hasCopyButton: true,// by default false
-          /* other options are:- 
+                  // prepare
+                  final bytes = utf8.encode(text);
+                  final blob = html.Blob([bytes]);
+                  final url = html.Url.createObjectUrlFromBlob(blob);
+                  final anchor =
+                      html.document.createElement('a') as html.AnchorElement
+                        ..href = url
+                        ..style.display = 'none'
+                        ..download = 'pipelinerun.yaml';
+                  html.document.body?.children.add(anchor);
+
+                  // download
+                  anchor.click();
+
+                  // cleanup
+                  html.document.body?.children.remove(anchor);
+                  html.Url.revokeObjectUrl(url);
+                }),
+            body: Center(
+              child: SingleChildScrollView(
+                child: AnySyntaxHighlighter(
+                  "${_file}",
+                  fontSize: 16,
+                  lineNumbers: false, // by default false
+                  theme: AnySyntaxHighlighterThemeCollection
+                      .defaultLightTheme(), // you can create and pass custom theme using AnySyntaxHighlighterTheme class
+                  isSelectableText:
+                      true, // this creates a SelectableText.rich() widget, makes text selectable (by default false)
+                  useGoogleFont: 'Source Code Pro',
+                  copyIcon: const Icon(Icons.copy_rounded,
+                      color: Colors.black), // default is white colored icon
+                  hasCopyButton: true, // by default false
+                  /* other options are:- 
           padding,
           margin,
           textAlign,
@@ -79,12 +106,10 @@ child: AnySyntaxHighlighter(
           textHeightBehavior,
           overrideDecoration
           */
-),
+                ),
 
-        //child: SelectableText("${_file}"),
-      ),
-    )
-    );
+                //child: SelectableText("${_file}"),
+              ),
+            ));
   }
-
 }
